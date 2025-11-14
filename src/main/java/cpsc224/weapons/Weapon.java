eddc3cf981211615bd8fe0abb441c39424a091b8
Ryan Heiser <rmheiser808@gmail.com>
2025-11-13 18:48:12 -0800

    Weapon class

--- src/main/java/cpsc224/weapons/Weapon.java
+++ src/main/java/cpsc224/weapons/Weapon.java
@@ -1,5 +1,28 @@
 package cpsc224.weapons;
 
+import cpsc224.moves.Move;
+
 public class Weapon {
 
+    private String name;
+    private int tier;
+    private Move move;
+
+    public Weapon(int tier, String name, Move move) {
+        this.tier = tier;
+        this.name = name;
+        this.move = move;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public Move getMove() {
+        return move;
+    }
 }
