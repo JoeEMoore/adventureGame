d59599aacac5516ab75d81c2b98f76dd510c3280
Ryan Heiser <rmheiser808@gmail.com>
2025-11-13 20:10:47 -0800

    ConsumableFactory with small health potion

---
+++ src/main/java/cpsc224/effects/HealEffect.java
@@ -0,0 +1,18 @@
+package cpsc224.effects;
+
+import cpsc224.creatures.Creature;
+
+public class HealEffect extends InstantEffect {
+
+    private int healAmount;
+
+    public HealEffect(int healAmount) {
+        this.healAmount = healAmount;
+    }
+
+    @Override
+    public void apply(Creature creature) {
+        // TODO
+    }
+
+}
--- src/main/java/cpsc224/items/consumables/Consumable.java
+++ src/main/java/cpsc224/items/consumables/Consumable.java
@@ -7,7 +7,18 @@ import cpsc224.effects.Effect;
 
 public class Consumable {
     
+    private String name;
+
     private List<Effect> effects;
+
+    public Consumable(String name, List<Effect> effects) {
+        this.name = name;
+        this.effects = effects;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public Collection<Effect> getEffects() {
         return effects;
