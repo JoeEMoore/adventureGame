244d58ebe842c7fb0dd92dc505ebc7ea6e269257
Ryan Heiser <rmheiser808@gmail.com>
2025-11-13 19:36:13 -0800

    Consumable changes

--- src/main/java/cpsc224/items/Consumable.java
+++ src/main/java/cpsc224/items/Consumable.java
@@ -8,17 +8,8 @@ import cpsc224.effects.Effect;
 public class Consumable {
     
     private List<Effect> effects;
-<<<<<<< HEAD
 
     public Collection<Effect> getEffects() {
         return effects;
     }
-=======
-    
-    public List<Effect> getEffects() {
-        return effects;
-    }
-
-    
->>>>>>> 2ade4f7e9026f2a6a46b8cf5744db3959f88ce34
 }
