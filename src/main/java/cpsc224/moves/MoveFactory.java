800aea834ebe2686d54727d9697901fd9eae6642
Ryan Heiser <rmheiser808@gmail.com>
2025-11-13 19:55:41 -0800

    Moved consumables into cpsc224.items.consumables and weapons into cpsc224.items.weapons

--- src/main/java/cpsc224/moves/MoveFactory.java
+++ src/main/java/cpsc224/moves/MoveFactory.java
@@ -2,7 +2,6 @@ package cpsc224.moves;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.DataFormatException;
 
 import cpsc224.DamageType;
 import cpsc224.effects.Effect;
@@ -10,14 +9,14 @@ import cpsc224.effects.Effect;
 public class MoveFactory {
 
     public static Move createSlashMove() {
-        String name = "Slash";
-        int damage = 10;
-        DamageType dt = DamageType.Slice;
-        int maxUses = -1;
-        double accuracy = 0.8;
-        boolean canTargetAllies = false;
-        boolean canTargetEnemies = true;
-        List<Effect> effects = new ArrayList<>();
+        final String name = "Slash";
+        final int damage = 10;
+        final DamageType dt = DamageType.Slice;
+        final int maxUses = -1;
+        final double accuracy = 0.8;
+        final boolean canTargetAllies = false;
+        final boolean canTargetEnemies = true;
+        final List<Effect> effects = new ArrayList<>();
 
         return new Move(name, damage, dt, maxUses, accuracy, canTargetAllies, canTargetEnemies, effects);
     }
