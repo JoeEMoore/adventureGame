5044e1adfb68690fcc89f027e8ba720ad8c2600d
JoeEMoore <jmoore27@zagmail.gonzaga.edu>
2025-11-13 19:51:16 -0800

    #

--- src/main/java/cpsc224/creatures/Creature.java
+++ src/main/java/cpsc224/creatures/Creature.java
@@ -1,24 +1,35 @@
 package cpsc224.creatures;
 
-import java.security.cert.CertPath;
-
 public abstract class Creature {
     private int health;
     private CreatureModifiers modifiers;
     private CreatureModifiers turnModifiers;
 
-    public
-    
-      public Creature(CreatureModifiers modifiers, int health){
+    public Creature(CreatureModifiers modifiers, int health) {
         this.modifiers = modifiers;
         this.health = health;
+
         
+        this.turnModifiers = new CreatureModifiers(0, 0, new double[0]);
     }
 
-
-    public int getHealth(){
+    public int getHealth() {
         return health;
     }
 
-    
+    public CreatureModifiers getModifiers() {
+        return modifiers;
+    }
+
+    public CreatureModifiers getTurnModifiers() {
+        return turnModifiers;
+    }
+
+    public void addHealth(int amount) {
+        this.health += amount;
+    }
+
+    public void takeDamage(int amount) {
+        this.health -= amount;
+    }
 }
