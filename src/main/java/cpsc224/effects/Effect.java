1a921212c78ad3bf853a96da8c9e563183b32f0e
Ryan Heiser <rmheiser808@gmail.com>
2025-11-13 18:24:21 -0800

    Effect class

--- src/main/java/cpsc224/effects/Effect.java
+++ src/main/java/cpsc224/effects/Effect.java
@@ -1,5 +1,14 @@
 package cpsc224.effects;
 
+import cpsc224.creatures.Creature;
+
 public abstract class Effect {
 
+    protected int turns;    
+
+    public Effect(int turns) {
+        this.turns = turns;
+    }
+
+    public abstract void apply(Creature creature);
 }
