5a90ec0bd9551f6e7dd20c817f06dfbb0e0f2c8c
Ty Glatt <tglatt@zagmail.gonzaga.edu>
2025-11-18 22:21:43 -0800

    basic Fight tests

---
+++ src/test/java/cpsc224/FightTests.java
@@ -0,0 +1,70 @@
+package cpsc224;
+
+import org.junit.jupiter.api.Test;
+
+import cpsc224.creatures.CreatureFactory;
+import cpsc224.creatures.Player;
+import cpsc224.items.weapons.WeaponFactory;
+import cpsc224.creatures.Creature;
+import cpsc224.items.weapons.Weapon;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FightTests {
+    
+    @Test
+    public void PerformingMoves (){
+        Player p = CreatureFactory.createPlayer();
+        Creature rat = CreatureFactory.createRat();
+        Weapon s = WeaponFactory.createDullSword();
+        Fight f = new Fight(p, rat, 1);
+        double intitialRhealth = rat.getHealth();
+        String t1 = f.performMove(p,rat,s);
+        double dmg = s.getMove().getDamage() * p.getTurnModifiers().getDamage();
+        rat.applyDamage(dmg, s.getMove().getDamageType());
+        double afterRhealth = rat.getHealth();
+
+        assertTrue(t1.contains(s.getName()));
+        assertTrue(t1.contains(rat.getName()));
+        assertTrue(t1.contains(p.getInventory().getWeapon(0).getName()));
+        assertTrue(t1.contains(p.getName()));
+        rat.getBaseModifiers().setEvasion(0);
+        assertTrue(afterRhealth < intitialRhealth);
+
+        assertEquals(s.getMove().createEffects().size(), rat.getEffects().size());
+        
+    }
+    
+    @Test
+    public void createsTurns () {
+        Player p = CreatureFactory.createPlayer();
+        Creature rat = CreatureFactory.createRat();
+        Fight f = new Fight(p, rat, 4348675);
+        String t1 = f.creatureTurn(p, rat);
+
+        assertTrue(t1.contains("Dull Sword"));
+        assertTrue(t1.contains("Rat"));
+        assertTrue(t1.contains(p.getInventory().getWeapon(0).getName()));
+        assertTrue(t1.contains("Player"));
+
+    }
+
+    @Test
+    public void createFights() {
+        Player p = CreatureFactory.createPlayer();
+        Creature rat = CreatureFactory.createRat();
+        Fight f = new Fight(p, rat, 4348675);
+        String o = f.creatureTurn(p, rat);
+
+        assertTrue(o.contains("Player"));
+        assertTrue(o.contains("Rat"));
+        assertTrue(o.contains(p.getInventory().getWeapon(0).getName()));
+        
+        
+
+    }
+
+}
