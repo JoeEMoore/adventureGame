a9e88cc9a91e59c4e502e03eebe768f3cccd0080
ORooff <orooff@zagmail.gonzaga.edu>
2025-11-20 20:42:09 -0800

    updated inventory ui, updateDisplay() method

--- src/main/java/cpsc224/dialogs/InventoryDialog.java
+++ src/main/java/cpsc224/dialogs/InventoryDialog.java
@@ -3,8 +3,11 @@ package cpsc224.dialogs;
 import cpsc224.creatures.Creature;
 import cpsc224.items.consumables.Consumable;
 import cpsc224.panels.GamePanel;
+import cpsc224.items.weapons.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 
 /**
@@ -16,16 +19,23 @@ public class InventoryDialog extends JDialog {
     private Creature creature;
     private GamePanel creaturePanel;
     private Consumable currentConsumable;
+    private Weapon currentWeapon;
 
     private JPanel dialogPanel;
     private JPanel buttonPanel;
+    private JPanel weaponPanel;
+    private JPanel consumablePanel;
 
     private JList<Consumable> consumableList;
-    DefaultListModel<Consumable> listModel;
+    private JList<Weapon> weaponList;
+    DefaultListModel<Consumable> consumableListModel;
+    DefaultListModel<Weapon> weaponListModel;
     private JScrollPane consumableScrollPane;
+    private JScrollPane weaponScrollPane;
 
     private JButton closeButton;
     private JButton useButton;
+    private JButton dropButton;
 
     /**
      * Creates the inventory dialog.
@@ -51,24 +61,40 @@ public class InventoryDialog extends JDialog {
      * Initializes the dialog's components
      */
     private void initComponents() {
-        listModel = new DefaultListModel<>();
+        consumableListModel = new DefaultListModel<>();
         for (Consumable cons : creature.getInventory().getConsumables()) {
-            listModel.addElement(cons);
+            consumableListModel.addElement(cons);
         }
 
-        consumableList = new JList<>(listModel);
+        consumableList = new JList<>(consumableListModel);
         consumableList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         consumableScrollPane = new JScrollPane(consumableList);
         consumableScrollPane.setPreferredSize(new Dimension(200, 100));
         consumableScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         consumableScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 
+        weaponListModel = new DefaultListModel<>();
+        for (Weapon weapon : creature.getInventory().getWeapons()) {
+            weaponListModel.addElement(weapon);
+        }
+
+        weaponList = new JList<>(weaponListModel);
+        weaponList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        weaponScrollPane = new JScrollPane(weaponList);
+        weaponScrollPane.setPreferredSize(new Dimension(200, 100));
+        weaponScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        weaponScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+
         useButton = new JButton("Use");
         useButton.setEnabled(false);
         closeButton = new JButton("Close");
+        dropButton = new JButton("Drop");
+        dropButton.setEnabled(false);
 
         buttonPanel = new JPanel();
         dialogPanel = new JPanel();
+        weaponPanel = new JPanel();
+        consumablePanel = new JPanel();
     }
 
     /**
@@ -78,11 +104,25 @@ public class InventoryDialog extends JDialog {
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
         buttonPanel.add(useButton);
         buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        buttonPanel.add(dropButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
         buttonPanel.add(closeButton);
 
-        dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
-        dialogPanel.add(consumableScrollPane);
-        dialogPanel.add(buttonPanel);
+        // Weapon panel
+        weaponPanel.setLayout(new BorderLayout());
+        weaponPanel.add(new JLabel("Weapons"), BorderLayout.NORTH);
+        weaponPanel.add(weaponScrollPane, BorderLayout.CENTER);
+
+        // Consumable panel
+        consumablePanel.setLayout(new BorderLayout());
+        consumablePanel.add(new JLabel("Consumables"), BorderLayout.NORTH);
+        consumablePanel.add(consumableScrollPane, BorderLayout.CENTER);
+
+        dialogPanel.setLayout(new BorderLayout());
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, weaponPanel, consumablePanel);
+
+        dialogPanel.add(splitPane, BorderLayout.CENTER);
+        dialogPanel.add(buttonPanel, BorderLayout.SOUTH);
 
         add(dialogPanel);
     }
@@ -98,7 +138,7 @@ public class InventoryDialog extends JDialog {
 
             // remove item
             creature.getInventory().getConsumables().remove(currentConsumable);
-            listModel.removeElement(currentConsumable);
+            consumableListModel.removeElement(currentConsumable);
 
             // reset
             currentConsumable = null;
@@ -110,6 +150,44 @@ public class InventoryDialog extends JDialog {
         consumableList.addListSelectionListener(e -> {
             currentConsumable = consumableList.getSelectedValue();
             useButton.setEnabled(currentConsumable != null);
+            dropButton.setEnabled(currentConsumable != null);
+            
+            if (currentConsumable != null) {
+                weaponList.clearSelection();
+                currentWeapon = null;
+            }
+        });
+
+        weaponList.addListSelectionListener(e -> {
+            currentWeapon = weaponList.getSelectedValue();
+            dropButton.setEnabled(currentWeapon != null);
+
+            if (currentWeapon != null) {
+                consumableList.clearSelection();
+                currentConsumable = null;
+            }
+        });
+
+        dropButton.addActionListener(e ->{
+            // Dropping a consumable
+            Consumable droppedConsumable = currentConsumable;
+            creature.getInventory().getConsumables().remove(currentConsumable);
+            consumableListModel.removeElement(currentConsumable);
+            creaturePanel.updateDisplay();
+
+            // reset
+            currentConsumable = null;
+            consumableList.clearSelection();   
+
+            // Dropping a weapon
+            Weapon droppedWeapon = currentWeapon;
+            creature.getInventory().getWeapons().remove(currentWeapon);
+            weaponListModel.removeElement(currentWeapon);
+            creaturePanel.updateDisplay();
+
+            // reset
+            currentWeapon = null;
+            weaponList.clearSelection();
         });
     }
 }
